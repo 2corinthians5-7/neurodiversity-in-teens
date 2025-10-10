e71c417a9d5b526f770262df3b73dff8eddd4913
Gillian Jiang <ggillianjiang@gmail.com>
2025-10-10 08:27:00 +0000

    done

--- neurodiversity-in-teenagers/vite.config.ts
+++ neurodiversity-in-teenagers/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/neurodiversity-in-teens/neurodiversity-in-teenagers/',
+    base: '/neurodiversity-in-teens/',
   build: {
     outDir: 'dist'
   }
