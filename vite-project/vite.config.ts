e2471d0ce770a89225f95a35752f1f0674525e24
Gillian Jiang <ggillianjiang@gmail.com>
2025-10-13 12:14:27 +0000

    test directory

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/neurodiversity-in-teens/vite-project/',
-  build: {
-    outDir: 'dist'
-  }
+  base: './',
 })
